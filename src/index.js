3046a552fba73d177ae544ae2b9478f1105f8319
Maaz Ahmad <60289705+MaazAhmad-tech@users.noreply.github.com>
2023-05-21 16:37:20 +0000

    Requiring react and react-dom

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//ReactDOM.render(WHAT TO RENDER, WHERE TO RENDER);
+ReactDOM.render(<h1>Hello Wordl!</h1>, document.getElementById("root"));
